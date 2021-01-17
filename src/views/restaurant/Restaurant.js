d8665128d218333c3ecf85287b7a3aa4b4051910
ernest <ernest2688@gmail.com>
2021-01-17 22:43:18 +0800

    Modify the trend part of restaurant dashboard

--- src/views/restaurant/Restaurant.js
+++ src/views/restaurant/Restaurant.js
@@ -88,10 +88,10 @@ const Restaurant = () => {
     </CRow>
 
      
-      <CCardGroup columns className = "cols-2" >
+    <CCol xs="12" md="10" className="mb-5">
         <CCard>
           <CCardHeader>
-            <strong>Domain Score</strong>
+            <h3><strong>Domain Score</strong></h3>
           </CCardHeader>
           <CCardBody>
             <CChartPolarArea
@@ -131,7 +131,8 @@ const Restaurant = () => {
             />
           </CCardBody>
         </CCard>
-      </CCardGroup>
+        </CCol>
+      
 
 
 
@@ -140,8 +141,8 @@ const Restaurant = () => {
         <CCardBody>
           <CRow>
             <CCol sm="5">
-              <h4 id="traffic" className="card-title mb-0">Traffic</h4>
-              <div className="small text-muted">November 2017</div>
+              <h4 id="traffic" className="card-title mb-0">Prediction of Performance</h4>
+              <div className="small text-muted">up to reviews on Dec 2020</div>
             </CCol>
             <CCol sm="7" className="d-none d-md-block">
               <CButton color="primary" className="float-right">
@@ -165,59 +166,7 @@ const Restaurant = () => {
           </CRow>
           <MainChartExample style={{height: '300px', marginTop: '40px'}}/>
         </CCardBody>
-        <CCardFooter>
-          <CRow className="text-center">
-            <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">Visits</div>
-              <strong>29.703 Users (40%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="success"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
-              <div className="text-muted">Unique</div>
-              <strong>24.093 Users (20%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="info"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">Pageviews</div>
-              <strong>78.706 Views (60%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="warning"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">New Users</div>
-              <strong>22.123 Users (80%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="danger"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
-              <div className="text-muted">Bounce Rate</div>
-              <strong>Average Rate (40.15%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                value={40}
-              />
-            </CCol>
-          </CRow>
-        </CCardFooter>
+        
       </CCard>
       
 
