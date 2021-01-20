05f0b926bb8f735288a74ed1cf54e214f4c74fd4
Jeffrey <jeffrey99930@gmail.com>
2021-01-20 21:15:33 +0800

    realm set up

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -19,9 +19,13 @@ import MainChartExample from '../charts/MainChartExample.js'
 import usersData from '../users/UsersData'
 import mongoData from '../users/MongoData'
 // import { DocsLink } from 'src/reusable'
+import * as Realm from "realm-web";
 
 const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
 const WidgetsBrand = lazy(() => import('../widgets/WidgetsBrand.js'))
+
+const app = new Realm.App({ id: "fyp_api-bhlis" });
+const credentials = Realm.Credentials.anonymous();
 
 
 const getBadge = status => {
@@ -33,47 +37,78 @@ const getBadge = status => {
     default: return 'primary'
   }
 }
-const fields = ['name','registered', 'role', 'status']
+const fields = ['name', 'registered', 'role', 'status']
 
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: []
+    };
+  }
 
-const Dashboard = () => {
-  return (
-    <>
-      <CRow>
-        <CCol>
-          <CCard>
-            <CCardHeader>
-              Combined All dark Table
+  async componentDidMount() {
+    try {
+      // Authenticate the user
+      const newuser = await app.logIn(Realm.Credentials.anonymous());
+      this.setState({
+        user: newuser
+      })
+    } catch (err) {
+      console.error("Failed to log in", err);
+    }
+    this.get_restaurant_info(); // after function a finished, this function will calls
+
+  }
+
+  get_restaurant_info = async () => {
+    const restaurant_info = await this.state.user.functions.get_restaurant_info_for_dashboard();
+    console.log(restaurant_info)
+  }
+
+  render() {
+    return (
+      <>
+        <CRow>
+          <CCol>
+            <CCard>
+              <CCardHeader>
+                Combined All dark Table
             </CCardHeader>
-            <CCardBody>
-            <CDataTable
-              items={mongoData}
-              fields={fields}
-              dark
-              hover
-              striped
-              bordered
-              size="sm"
-              itemsPerPage={10}
-              pagination
-              scopedSlots = {{
-                'status':
-                  (item)=>(
-                    <td>
-                      <CBadge color={getBadge(item.status)}>
-                        {item.status}
-                      </CBadge>
-                    </td>
-                  )
-              }}
-            />
-            </CCardBody>
-          </CCard>
-        </CCol>
-      </CRow>
-      
-    </>
-  )
+              <CCardBody>
+                <CDataTable
+                  items={mongoData}
+                  fields={fields}
+                  dark
+                  hover
+                  striped
+                  bordered
+                  size="sm"
+                  itemsPerPage={10}
+                  pagination
+                  scopedSlots={{
+                    'status':
+                      (item) => (
+                        <td>
+                          <CBadge color={getBadge(item.status)}>
+                            {item.status}
+                          </CBadge>
+                        </td>
+                      )
+                  }}
+                />
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+  
+      </>
+    )
+  }
+
+
+
+
 }
 
 export default Dashboard
