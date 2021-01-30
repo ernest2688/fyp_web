56472f9984c00e1b542f41af17431bc5ab58f997
ernest <ernest2688@gmail.com>
2021-01-31 00:07:01 +0800

    restaurant error solved

--- src/views/restaurant/Restaurant.js
+++ src/views/restaurant/Restaurant.js
@@ -1,4 +1,4 @@
-import React, { lazy , useState, useEffect } from 'react'
+import React, { lazy , useState, useEffect ,useRef} from 'react'
 import { useParams } from "react-router-dom";
 import {
   CBadge,
@@ -52,6 +52,7 @@ const Restaurant = () => {
     }
 
     const restaurant_info = await user.functions.get_detail_restaurant_info(name);
+    console.log(restaurant_info);
     
   });
   const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.'
