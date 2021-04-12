691edfd7621c544c2a2c7325a9157f47ef857059
Liu Tsz Hei <jeffrey99930@gmail.com>
2021-04-13 01:16:53 +0800

    changed dashboard UI

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -35,14 +35,15 @@ const getBadge = status => {
   }
 }
 
-const fields = ['name','district','categories','Last_Week_Avg_Score', 'This_Week_Avg_Score','prediction','most_common_hashtag','total_num_of_posts',
-{
-  key: 'show_details',
-  label: '',
-  _style: { width: '1%' },
-  sorter: false,
-  filter: false
-}]
+//const fields = ['name','district','categories','Last_Week_Avg_Score', 'This_Week_Avg_Score','prediction','most_common_hashtag','total_num_of_posts',
+const fields = ['name', 'district', 'categories', 'This_Week_Avg_Score', 'one_Week_change', 'prediction', 'Average_env_score', 'Average_food_score', 'Average_service_score', 'most_common_hashtag', 'total_num_of_posts',
+  {
+    key: 'show_details',
+    label: '',
+    _style: { width: '1%' },
+    sorter: false,
+    filter: false
+  }]
 
 
 
@@ -60,7 +61,7 @@ class Dashboard extends React.Component {
 
   }
 
-  handleClick(item,index){
+  handleClick(item, index) {
     console.log(item.name);
   }
 
@@ -83,52 +84,52 @@ class Dashboard extends React.Component {
     //console.log(restaurant_info)
 
     restaurant_info.info.forEach(obj => {
-        var categories = '';
-        obj['categories'].forEach(oobj => {
-            categories = categories + oobj + '/'
+      var categories = '';
+      obj['categories'].forEach(oobj => {
+        categories = categories + oobj + '/'
       })
       categories = categories.substring(0, categories.length - 1);
       obj['categories'] = categories
 
     });
 
-
-    restaurant_info.info.forEach(e => {
+    var undefined_rest = []
+    restaurant_info.info.forEach(function (e, i, o) {
       let nid_tag = e.nid.split('(')[0];
       nid_tag = nid_tag.trim();
 
       e.total_num_of_posts = e.reviewCount;
-      restaurant_info.most_common_hashtags.forEach( k=>{
+      restaurant_info.most_common_hashtags.forEach(k => {
         if (k.OpenRice_Restaurant_name.toString().split('(')[0].trim() === nid_tag) {
-          try{
-            e.most_common_hashtag = Object.keys(k.Top_Hashtags[0])[0] ;
+          try {
+            e.most_common_hashtag = Object.keys(k.Top_Hashtags[0])[0];
           }
-          catch{
-            e.most_common_hashtag = "null" ;
+          catch {
+            e.most_common_hashtag = "null";
           }
-          return  
+          return
         }
       })
-      restaurant_info.latest_week_score.forEach( k=>{
+      restaurant_info.latest_week_score.forEach(k => {
         if (k.Openrice_Restaurant_Name.toString().split('(')[0].trim() === nid_tag) {
-          e.Last_Week_Avg_Score = k.Date_and_Scores[0].Average_score;
-          e.This_Week_Avg_Score = k.Date_and_Scores[1].Average_score;
-
-          e.Average_Eng_and_emoji_score = k.Date_and_Scores[1].Average_Eng_and_emoji_score;
-          e.Average_env_score = k.Date_and_Scores[1].Average_env_score;
-          e.Average_food_score = k.Date_and_Scores[1].Average_food_score;
-          e.Average_score = k.Date_and_Scores[1].Average_score;
-          e.Average_service_score = k.Date_and_Scores[1].Average_service_score;
+          //          e.Last_Week_Avg_Score = k.Date_and_Scores[0].Average_score;
+          e.This_Week_Avg_Score = k.Date_and_Scores[1].Average_score.toFixed(3);
+          e.one_Week_change = (k.Date_and_Scores[1].Average_score - k.Date_and_Scores[0].Average_score).toFixed(3);
+          e.Average_Eng_and_emoji_score = k.Date_and_Scores[1].Average_Eng_and_emoji_score.toFixed(3);
+          e.Average_env_score = k.Date_and_Scores[1].Average_env_score.toFixed(3);
+          e.Average_food_score = k.Date_and_Scores[1].Average_food_score.toFixed(3);
+          e.Average_score = k.Date_and_Scores[1].Average_score.toFixed(3);
+          e.Average_service_score = k.Date_and_Scores[1].Average_service_score.toFixed(3);
           e.Date = k.Date_and_Scores[1].Date.split('T')[0];
           return
         }
       })
-      restaurant_info.prediction.forEach( k=>{
+      restaurant_info.prediction.forEach(k => {
         if (k.Openrice_Restaurant_Name.toString().split('(')[0].trim() === nid_tag) {
-          if (k.Average_score){
+          if (k.Average_score == true) {
             e.prediction = "increase";
           }
-          else if(!k.Average_score){
+          else if (k.Average_score == false) {
             e.prediction = "decrease";
           }
           else {
@@ -137,14 +138,18 @@ class Dashboard extends React.Component {
           return
         }
       })
+
     });
-      
-      
+
+    restaurant_info.info = restaurant_info.info.filter(i => i.This_Week_Avg_Score != null);
+    restaurant_info.info = restaurant_info.info.filter(i => i.prediction != null);
+    
+    console.log(restaurant_info.info)
     this.setState({
       restaurant_info: restaurant_info.info,
-      filtered_info : restaurant_info.info
+      filtered_info: restaurant_info.info
     })
-    //console.log(this.state.filtered_info)
+    console.log(this.state.filtered_info)
   }
 
   toggleDetails(index) {
@@ -165,22 +170,22 @@ class Dashboard extends React.Component {
 
     return (
       <>
-            
+
         <CJumbotron className="border">
           <h1 className="display-3">Welcome to the new restaurant review system</h1>
           <h3 className="lead">In here you can have a peek on the performance of each restaurants.</h3>
           <hr className="my-2" />
           <h3>Below is the dashboard of the restaurants.</h3>
         </CJumbotron>
-            
-        
+
+
         <CRow>
           <CCol>
             <CCard>
               <CCardHeader>
                 <h2>Restaurants</h2>
-                
-            </CCardHeader>
+
+              </CCardHeader>
               <CCardBody>
                 <CDataTable
                   items={this.state.filtered_info}
@@ -198,50 +203,50 @@ class Dashboard extends React.Component {
                   pagination
                   scopedSlots={{
                     'show_details':
-                    (item, index)=>{
-                    return (
-                      <td className="py-2">
-                        <CButton
-                        color="primary"
-                        variant="outline"
-                        shape="square"
-                        size="sm"
-                        onClick={()=>{this.toggleDetails(index)}}
-                        >
-                         {this.state.details.includes(index) ? 'Hide' : 'Show'}
-                         </CButton>
-                      </td>
-                    )
-                    },
+                      (item, index) => {
+                        return (
+                          <td className="py-2">
+                            <CButton
+                              color="primary"
+                              variant="outline"
+                              shape="square"
+                              size="sm"
+                              onClick={() => { this.toggleDetails(index) }}
+                            >
+                              {this.state.details.includes(index) ? 'Hide' : 'Show'}
+                            </CButton>
+                          </td>
+                        )
+                      },
                     'details':
-                      (item, index)=>{
-                       return (
-                       <CCollapse show={this.state.details.includes(index)}>
-                         <CCardBody>
-                           <h4>Scores:</h4>
-                           <div style={{display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-between'}}>
-                           <h5>Food: {item.Average_food_score}</h5>
-                           <h5>Environment: {item.Average_env_score}</h5>
-                           <h5>Service: {item.Average_service_score}</h5>
-                           <h5>Emoji Score: {item.Average_Eng_and_emoji_score}</h5>
-                           <h5>Total Score: {item.Average_score}</h5>
-                           </div>
-                           <p className="text-muted">Update since: {item.Date}</p>
-                           <CButton size="sm" color="info">
-                              <Link to={`/restaurant/${item.nid}`}><strong>Show Details</strong></Link>
-                           </CButton>
-                         </CCardBody>
-                       </CCollapse>
-                      )
-                    }
+                      (item, index) => {
+                        return (
+                          <CCollapse show={this.state.details.includes(index)}>
+                            <CCardBody>
+                              <h4>Scores:</h4>
+                              <div style={{ display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-between' }}>
+                                <h5>Food: {item.Average_food_score}</h5>
+                                <h5>Environment: {item.Average_env_score}</h5>
+                                <h5>Service: {item.Average_service_score}</h5>
+                                <h5>Emoji Score: {item.Average_Eng_and_emoji_score}</h5>
+                                <h5>Total Score: {item.Average_score}</h5>
+                              </div>
+                              <p className="text-muted">Update since: {item.Date}</p>
+                              <CButton size="sm" color="info">
+                                <Link to={`/restaurant/${item.nid}`}><strong>Show Details</strong></Link>
+                              </CButton>
+                            </CCardBody>
+                          </CCollapse>
+                        )
+                      }
 
-                   }}
+                  }}
                 />
               </CCardBody>
             </CCard>
           </CCol>
         </CRow>
-  
+
       </>
     )
   }
